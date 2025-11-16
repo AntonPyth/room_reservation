c0c389be37f77029cd36d51cf42a48e58283b07b
Anton K <executor1@gmail.com>
2025-11-17 02:54:07 +0300

    finishing previous theme

---
+++ alembic/versions/01-2025_11_17_0250-initial_structure.py
@@ -0,0 +1,24 @@
+"""Initial structure
+
+Revision ID: 01
+Revises: b218f122b8c1
+Create Date: 2025-11-17 02:50:41.747065
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '01'
+down_revision = 'b218f122b8c1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
